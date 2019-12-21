4a7544035f350b911be125aaae25ea7e0b2780e8
thomas dubiel <td1729@hotmail.com>
2019-12-21 14:35:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(deli)
   if !deli.empty? 
     puts "The line is currently:" 
     deli.each_with_index do |name, index| 
-      print "#{index + 1}. #{name}" 
+      print "#{index + 1}. #{name} " 
     end 
   else 
     puts "The line is currently empty." 
