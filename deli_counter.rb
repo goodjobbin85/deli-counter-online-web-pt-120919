bbc9866d6fab240295ef43495cc4bd5071a05ca7
thomas dubiel <td1729@hotmail.com>
2019-12-21 15:37:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(deli)
     final_line = "The line is currently: " 
     deli.each_with_index do |name, index| 
       final_line << "#{index + 1}. #{name} " 
-      if index == -1
+      if deli[index].index == -1
         final_line << "#{index + 1}. #{name}" 
       end
     end 
